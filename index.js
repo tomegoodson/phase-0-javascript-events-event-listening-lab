0b85d82ef4bbfa87b642420f1c0deb704db7fcd1
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-13 16:19:57 -0500

    eventlab.md

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
-}
+    const myButton = document.getElementById('button');
+    if (myButton) {
+      myButton.addEventListener('click', function() {
+        console.log('Button was clicked!');
+      });
+    }
+  }
+  
+  // Ensure that addingEventListener gets called when the document is fully loaded
+  document.addEventListener('DOMContentLoaded', (event) => {
+    addingEventListener();
+  });
+  
